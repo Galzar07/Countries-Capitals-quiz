e5ac40a84e01d274373f543988f26b324db53d26
Galzar07 <52508198+Galzar07@users.noreply.github.com>
2020-05-09 12:49:28 +0200

    Updates

---
+++ precache-manifest.2c42aed410a8f9faadedb06f981ffa13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6589121c6db1421de37b171974abe532",
+    "url": "/Countries-Capitals-quiz/index.html"
+  },
+  {
+    "revision": "463f28eb0d55defe6372",
+    "url": "/Countries-Capitals-quiz/static/css/main.d6abe205.chunk.css"
+  },
+  {
+    "revision": "fc4ef50a2c29d6961ed0",
+    "url": "/Countries-Capitals-quiz/static/js/2.47f7773f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Countries-Capitals-quiz/static/js/2.47f7773f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "463f28eb0d55defe6372",
+    "url": "/Countries-Capitals-quiz/static/js/main.7398f610.chunk.js"
+  },
+  {
+    "revision": "44b1eab25b254a3179c5",
+    "url": "/Countries-Capitals-quiz/static/js/runtime-main.0a66c321.js"
+  },
+  {
+    "revision": "b2d1d447e4f9a12c1848a15034937284",
+    "url": "/Countries-Capitals-quiz/static/media/back.b2d1d447.jpg"
+  }
+]);
